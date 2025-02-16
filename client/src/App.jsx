5a2c6fad68245b601a86e624cd7b8970d0bcb97e
leapkore <leapkoretechnology@gmail.com>
2025-02-16 13:11:24 +0530

    Basic pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 import React from 'react'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/signin" element={<SignIn/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
