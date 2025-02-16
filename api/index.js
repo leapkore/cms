d995b5521fb3c46d46c5388b38234f5c347a7ee3
leapkore <leapkoretechnology@gmail.com>
2025-02-16 19:59:18 +0530

    Database Connected Mongo

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Database is connected");
+});
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on PORT 3000');
+  console.log("Server is running on PORT 3000");
 });
